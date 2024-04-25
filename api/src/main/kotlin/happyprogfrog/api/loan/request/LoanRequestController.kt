777cc204d5e1b7810d4b04bcb836eb8a515f20d3
happyprogfrog <happyprogfrog@gmail.com>
2024-04-25 15:06:09 +0900

    fix: 잘못된 URL 수정

--- api/src/main/kotlin/happyprogfrog/api/loan/request/LoanRequestController.kt
+++ api/src/main/kotlin/happyprogfrog/api/loan/request/LoanRequestController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/fintect/api/v1")
+@RequestMapping("/fintech/api/v1")
 class LoanRequestController(
     private val loanRequestServiceImpl: LoanRequestServiceImpl
 ) {
--- api/src/main/kotlin/happyprogfrog/api/loan/review/LoanReviewController.kt
+++ api/src/main/kotlin/happyprogfrog/api/loan/review/LoanReviewController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/fintenct/api/v1")
+@RequestMapping("/fintech/api/v1")
 class LoanReviewController(
     private val loanReviewServiceImpl: LoanReviewServiceImpl
 ){
