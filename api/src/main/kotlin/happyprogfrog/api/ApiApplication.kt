c0ba9700de47cce7fa1deec2f76169d7303723c8
happyprogfrog <happyprogfrog@gmail.com>
2024-05-24 11:34:19 +0900

    feat: 대출 심사 결과 응답 시 Redis를 사용하도록 변경

--- api/src/main/kotlin/happyprogfrog/api/ApiApplication.kt
+++ api/src/main/kotlin/happyprogfrog/api/ApiApplication.kt
@@ -3,11 +3,13 @@ package happyprogfrog.api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 @EntityScan(basePackages = ["happyprogfrog.domain"])
 @ComponentScan(basePackages = ["happyprogfrog"])
+@EnableCaching
 class ApiApplication
 
 fun main(args: Array<String>) {
--- api/src/main/kotlin/happyprogfrog/api/loan/review/LoanReviewServiceImpl.kt
+++ api/src/main/kotlin/happyprogfrog/api/loan/review/LoanReviewServiceImpl.kt
@@ -4,6 +4,7 @@ import happyprogfrog.api.exception.CustomErrorCode
 import happyprogfrog.api.exception.CustomException
 import happyprogfrog.domain.domain.LoanReview
 import happyprogfrog.domain.repository.LoanReviewRepository
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
 @Service
@@ -18,6 +19,7 @@ class LoanReviewServiceImpl(
         )
     }
 
+    @Cacheable(value = ["REVIEW"], key = "#userKey", cacheManager = "redisCacheManager")
     override fun getLoanResult(userKey: String) =
         loanReviewRepository.findByUserKey(userKey)
 
